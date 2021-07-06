fa907ec05196b75266eb423be75a4807b6190018
Frank Wei <frank@frankwei.xyz>
2021-07-06 22:08:34 +0800

    go typescript

--- test/contentKey.ts
+++ test/contentKey.ts
@@ -1,15 +1,15 @@
-const { expect } = require("chai");
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import type { Signer, Contract } from "ethers";
 
 describe("ContentKey", function () {
-  let accounts
-  let contentKey
+  let accounts: Signer[];
+  let contentKey: Contract;
 
   beforeEach(async function () {
     accounts = await ethers.getSigners();
 
-    const ContentKey = await ethers.getContractFactory(
-      "ContentKey",
-    );
+    const ContentKey = await ethers.getContractFactory("ContentKey");
     const ck = await ContentKey.deploy();
 
     contentKey = await ck.deployed();
@@ -22,8 +22,9 @@ describe("ContentKey", function () {
       transferable: true,
       contentHash: "foobar",
     };
-    const tx = await contentKey.mint(accounts[0].address, keyData);
+    const addr = await accounts[0].getAddress();
+    const tx = await contentKey.mint(addr, keyData);
     await tx.wait();
-    expect((await contentKey.balanceOf(accounts[0].address)).toNumber()).to.greaterThan(0);
+    expect((await contentKey.balanceOf(addr)).toNumber()).to.greaterThan(0);
   });
 });
